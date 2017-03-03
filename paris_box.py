b1e38a7148428756155c72f051b5edee77c8dd65
xavier bollart <xavier.bollart@gmail.com>
2017-03-03 11:46:49 -0400

    add parking script

--- paris_box.py
+++ paris_box.py
@@ -5,31 +5,22 @@ import utils.mail as mail
 
 if __name__ == '__main__':
     # set request parameters
-    date = datetime.now()
-    date = date - timedelta(days=1)
+    date = datetime.now() - timedelta(days=1)
     date = datetime(date.year, date.month, date.day)
-    p_max_by_m2 = 0
-    s_min = 0
-    s_max = 0
-    p_min = 0
-    p_max =60000
- #   category = "ventes_immobilieres"
- #   region = "ile_de_france"
-  #  location = "Paris"
- #   immo_type = "parking"
     to = "xavier.bollart@gmail.com"
-
     param_dict = {}
     param_dict['category'] = 'ventes_immobilieres'
     param_dict['region'] = 'ile_de_france'
     param_dict['location'] = 'Paris'
     param_dict['immo_type'] = 'parking'
-  #  param_dict['to'] = 'xavier.bollart@gmail.com'
+    param_dict['date'] = date
+    param_dict['p_max'] = 60000
+    param_dict['p_min'] = 0
+    param_dict['s_min'] = 0
+    param_dict['s_max'] = 25
+    param_dict['p_by_meter_max'] = 3000
 
-
-
-
-    ads_details = leboncoin.get_ads_info(param_dict, date, p_min, p_max, s_min, s_max, p_max_by_m2)
+    ads_details = leboncoin.get_ads_info(param_dict)
     
     # send Mail
     body = mail.build_body(ads_details)
